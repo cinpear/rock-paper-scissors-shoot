4350994940140f1748357ed5eb23f5805217f511
cinpear <cinders.pear@gmail.com>
2025-12-07 14:17:11 -0800

    fixed shop + sometimes you get a gun

--- control.gd
+++ control.gd
@@ -2,8 +2,9 @@ extends Node
 
 @onready var rng = RandomNumberGenerator.new()
 var wins = 0.0
-var nextPowerup = [10, 20, 30, 40, 60, 80, 160, 320, 640]
+var nextPowerup = [2, 20, 30, 40, 60, 80, 160, 320, 640]
 var nextPowerupIndex = 0
+var allPowerupsBought = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -15,63 +16,81 @@ func _process(delta: float) -> void:
 	pass
 
 func pause_one_sec():
-	$HBoxContainer/Button.disabled = true
-	$HBoxContainer/Button2.disabled = true
-	$HBoxContainer/Button3.disabled = true
+	$game/HBoxContainer/Button.disabled = true
+	$game/HBoxContainer/Button2.disabled = true
+	$game/HBoxContainer/Button3.disabled = true
 	await get_tree().create_timer(1.0).timeout
-	$HBoxContainer/Button.disabled = false
-	$HBoxContainer/Button2.disabled = false
-	$HBoxContainer/Button3.disabled = false
+	$game/HBoxContainer/Button.disabled = false
+	$game/HBoxContainer/Button2.disabled = false
+	$game/HBoxContainer/Button3.disabled = false
 
 func update_wins():
-	$Label.text = "Wins: " + str(wins)
-	if wins >= nextPowerup[nextPowerupIndex]:
-		roll_powerup()
-	$Label3.text = "Next Power Up: " + str(nextPowerup[nextPowerupIndex] - wins) + " wins"
+	$game/Label.text = "Wins: " + str(wins)
 	if wins >= 1000:
 		print("You win!")
+	if allPowerupsBought:
+		return
+	if wins >= nextPowerup[nextPowerupIndex]:
+		roll_powerup()
+	$game/Label3.text = "Next Power Up: " + str(nextPowerup[nextPowerupIndex] - wins) + " wins"
 
 func roll_powerup():
+	print("here")
+	$powerup.visible = true
+	$ColorRect.visible = true
 	if nextPowerupIndex < 9:
 		nextPowerupIndex += 1
-		$Label3.text = "Next Power Up: " + str(nextPowerup[nextPowerupIndex] - wins) + " wins"
-		print("congrats you get a powerup in my heart!")
+		if nextPowerupIndex == 9:
+			allPowerupsBought = true
+			$game/Label3.text = "Next Power Up: -"
+		else:
+			$game/Label3.text = "Next Power Up: " + str(nextPowerup[nextPowerupIndex] - wins) + " wins"
 
 # Chose rock
 func _on_button_pressed() -> void:
 	var move = rng.randi_range(1, 3)
+	var gun = rng.randi_range(1, 100)
+	if (gun <= global.gunChance):
+		$game/Label4.text = "Gun! 🔫"
+		$game/Label5.text = "Win!"
+		wins += 1.0
 	# rock
-	if (move == 1):
-		$Label4.text = "Rock"
-		$Label5.text = "Tie"
-		wins += 0.5
+	elif (move == 1):
+		$game/Label4.text = "Rock"
+		$game/Label5.text = "Tie"
+		wins += 1.0
 	# paper
 	elif (move == 2):
-		$Label4.text = "Paper"
-		$Label5.text = "Loss"
+		$game/Label4.text = "Paper"
+		$game/Label5.text = "Loss"
 		wins -= 1.0
 	# scissors
 	else:
-		$Label4.text = "Scissors"
-		$Label5.text = "Win!"
-		wins += 1.0
+		$game/Label4.text = "Scissors"
+		$game/Label5.text = "Win!"
+		wins += 2.0
 	update_wins()
 	pause_one_sec()
 
 # Chose paper
 func _on_button_2_pressed() -> void:
 	var move = rng.randi_range(1, 3)
-	if (move == 1):
-		$Label4.text = "Rock"
-		$Label5.text = "Win!"
+	var gun = rng.randi_range(1, 100)
+	if (gun <= global.gunChance):
+		$game/Label4.text = "Gun! 🔫"
+		$game/Label5.text = "Win!"
+		wins += 1.0
+	elif (move == 1):
+		$game/Label4.text = "Rock"
+		$game/Label5.text = "Win!"
 		wins += 1.0
 	elif (move == 2):
-		$Label4.text = "Paper"
-		$Label5.text = "Tie"
+		$game/Label4.text = "Paper"
+		$game/Label5.text = "Tie"
 		wins += 0.5
 	else:
-		$Label4.text = "Scissors"
-		$Label5.text = "Loss"
+		$game/Label4.text = "Scissors"
+		$game/Label5.text = "Loss"
 		wins -= 1.0
 	update_wins()
 	pause_one_sec()
@@ -79,17 +98,22 @@ func _on_button_2_pressed() -> void:
 # Chose scissors
 func _on_button_3_pressed() -> void:
 	var move = rng.randi_range(1, 3)
-	if (move == 1):
-		$Label4.text = "Rock"
-		$Label5.text = "Loss"
+	var gun = rng.randi_range(1, 100)
+	if (gun <= global.gunChance):
+		$game/Label4.text = "Gun! 🔫"
+		$game/Label5.text = "Win!"
+		wins += 1.0
+	elif (move == 1):
+		$game/Label4.text = "Rock"
+		$game/Label5.text = "Loss"
 		wins -= 1.0
 	elif (move == 2):
-		$Label4.text = "Paper"
-		$Label5.text = "Win!"
+		$game/Label4.text = "Paper"
+		$game/Label5.text = "Win!"
 		wins += 1.0
 	else:
-		$Label4.text = "Scissors"
-		$Label5.text = "Tie"
+		$game/Label4.text = "Scissors"
+		$game/Label5.text = "Tie"
 		wins += 0.5
 	update_wins()
 	pause_one_sec()
---
+++ global.gd
@@ -0,0 +1,8 @@
+extends Node
+
+var powerupBought = [false, false, false, false, false, false, false, false, false]
+var tieAmount = 0.5
+var lossAmount = -1.0
+var winAmount = 1.0
+var winMult = 1.0
+var gunChance = 50
